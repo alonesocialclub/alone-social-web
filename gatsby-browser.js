1fc008dcfb2a3cc8ebe6eea2afda0330c70d9957
holdonnn <rururu0729@gmail.com>
2019-09-15 20:53:31 +0900

    Setup GA

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,13 +4,10 @@ import "./src/styles/global.css"
 import "./src/styles/spoqa-han-sans.css"
 
 
-ReactGA.initialize('UA-77395473-1');
+ReactGA.initialize('UA-148032267-1');
 
 export const onRouteUpdate = (state, page, pages) => {
-  if ( state.location.search.includes('isAdmin') ) {
-    window.localStorage.setItem('isAmdin', 'true');
-  }
-  if (process.env.NODE_ENV === 'production' && !window.localStorage.getItem('isAmdin')) {
+  if (process.env.NODE_ENV === 'production') {
     ReactGA.pageview(state.location.pathname);
   }
 };
