71a9cd13ac51eee5fcf3725390375bc9436c0537
Suyeol Jeon <devxoul@gmail.com>
2019-09-15 20:46:28 +0900

    Add more paddings

--- src/component/Layout.js
+++ src/component/Layout.js
@@ -5,7 +5,7 @@ import { media } from '../utils/media'
 const Warpper = styled.div`
   padding: 50px;
   transition: all 0.3s ease;
-  ${media.mobile`padding: 30px 20px;`}
+  ${media.mobile`padding: 45px 30px;`}
 `
 const Content = styled.div`
   margin: 0 auto;
